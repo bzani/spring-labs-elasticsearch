0bbc73d57f25126bb62d2bbbbb1e3826d50379ee
bzani <ubzani@gmail.com>
2022-07-13 01:59:27 +0100

    config with properties

--- src/main/java/com/bdpz/config/ElasticsearchConfig.java
+++ src/main/java/com/bdpz/config/ElasticsearchConfig.java
@@ -1,21 +1,30 @@
 package com.bdpz.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SuppressWarnings("deprecation")
+@EnableElasticsearchRepositories(basePackages = "com.bdpz.repository")
 @Configuration
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
+	@Value("${elasticsearch.host:localhost}")
+	private String host;
+	
+	@Value("${elasticsearch.port:9200}")
+	private String port;
+	
 	@Bean
     public RestHighLevelClient elasticsearchClient() {
 
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
+            .connectedTo(host + ":" + port)
             .build();
 
         return RestClients.create(clientConfiguration).rest();
